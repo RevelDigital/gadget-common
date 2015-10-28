e5df510c25352a449d996bf951213431afa9f014
mike_tinnes@hotmail.com <tinman0>
2015-10-28 15:56:27 -0500

    updated regex

--- utils.js
+++ utils.js
@@ -1,5 +1,5 @@
 function getFamilyName(css) {
-  var regex = /font-family:\s*(.+?)\s*;/; 
+  var regex = /font-family:\s*['"]*(.+?)['"]*\s*;/;
   if(regex.test(css)) {
     var matches = css.match(regex);
     return matches[1].split(',')[0].trim();
